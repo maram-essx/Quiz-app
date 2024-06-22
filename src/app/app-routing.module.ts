3ab588a1204d5044176555df1deaa2b55befeab6
maram-essx <maramessam381@gmail.com>
2024-06-22 23:18:03 +0300

    structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }, { path: 'instructor', loadChildren: () => import('./features/instructor/instructor.module').then(m => m.InstructorModule) }, { path: 'students', loadChildren: () => import('./features/students/students.module').then(m => m.StudentsModule) }, { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
