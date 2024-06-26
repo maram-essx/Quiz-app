bffe5e798c0a3b9c1dd1e9fc2e29a6657c1c7fd5
maram-essx <maramessam381@gmail.com>
2024-06-26 21:03:20 +0300

    structer-g

--- src/app/features/instructor/groups/models/group.ts
+++ src/app/features/instructor/groups/models/group.ts
@@ -1,2 +1,36 @@
-export interface Group {
+export type IGroupsListRes = IGroupsListRes2[]
+
+export interface IGroupsListRes2 {
+  _id: string;
+  name: string;
+  status: string;
+  instructor: string;
+  students: string[];
+  max_students: number;
+}
+
+export interface IGroupDetailsRes {
+  _id: string
+  name: string
+  status: string
+  instructor: string
+  students: IStudent[]
+  max_students: number
+}
+
+export interface IStudent {
+  _id: string
+  first_name: string
+  last_name: string
+  email: string
+}
+
+export interface IUpdateOrAddGroup{
+  name: string
+  students: string[]
+}
+
+export interface IUdateGroupRes {
+  data: IGroupDetailsRes
+  message: string
 }
--- src/app/features/instructor/groups/services/group.service.ts
+++ src/app/features/instructor/groups/services/group.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IGroupsListRes, IGroupDetailsRes, IUpdateOrAddGroup, IUdateGroupRes } from '../models/group';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
 
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) { }
+
+  getAllGroups(): Observable<IGroupsListRes> {
+    return this._HttpClient.get<IGroupsListRes>(`group`)
+  }
+
+  getGroupById(id:string):Observable<IGroupDetailsRes>{
+    return this._HttpClient.get<IGroupDetailsRes>(`group/${id}`)
+  }
+  deleteGroup(id:string):Observable<IGroupDetailsRes>{
+    return this._HttpClient.delete<IGroupDetailsRes>(`group/${id}`)
+  }
+  
+  editGroup(id:string, editData:IUpdateOrAddGroup):Observable<IUdateGroupRes>{
+    return this._HttpClient.put<IUdateGroupRes>(`group/${id}`,editData)
+  }
+  AddNewGroup(addNewGroup:IUpdateOrAddGroup):Observable<any>{
+    return this._HttpClient.post<IUdateGroupRes>(`group`,addNewGroup)
+  }
 }
