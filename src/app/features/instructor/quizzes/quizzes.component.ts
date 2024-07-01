55d224bd2ed70cc8c1ddbf419aa6b58c3942e5f5
IsrAhmad <Israa.099@gmail.com>
2024-07-01 11:43:40 +0300

    table designs

--- src/app/features/instructor/quizzes/quizzes.component.ts
+++ src/app/features/instructor/quizzes/quizzes.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { IQuizzes } from './models/iQuizzes';
+import { QuizzesService } from './services/quizzes.service';
 
 @Component({
   selector: 'app-quizzes',
   templateUrl: './quizzes.component.html',
-  styleUrls: ['./quizzes.component.scss']
+  styleUrls: ['./quizzes.component.scss'],
 })
 export class QuizzesComponent {
+  allQuizzes: IQuizzes[] = [];
+  quizList: any[] = [];
 
+  constructor(private _QuizzesService: QuizzesService) {}
 }
