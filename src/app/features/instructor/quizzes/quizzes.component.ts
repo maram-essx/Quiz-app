5d1d6712fdbf82d68fc32fc5c27e7c89565089eb
IsrAhmad <Israa.099@gmail.com>
2024-07-01 13:02:46 +0300

    UI done but group names returns group ID instead

--- src/app/features/instructor/quizzes/quizzes.component.ts
+++ src/app/features/instructor/quizzes/quizzes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IQuizzes } from './models/iQuizzes';
 import { QuizzesService } from './services/quizzes.service';
+import { IQuiz } from '../models/instructor';
 
 @Component({
   selector: 'app-quizzes',
@@ -10,6 +11,42 @@ import { QuizzesService } from './services/quizzes.service';
 export class QuizzesComponent {
   allQuizzes: IQuizzes[] = [];
   quizList: any[] = [];
+  completedQuizList: any[] =[];
+  openQuizList: any[] =[];
+  upcomingQuizzes: IQuiz[] = [];
+  defaultImg: string = '../../../../assets/images/Default-img.svg'
 
   constructor(private _QuizzesService: QuizzesService) {}
+
+  ngOnInit() {
+    this.upComingExams();
+    this.onAllQuizzes();
+    this.completedQuizzes();
+  }
+
+  upComingExams(): void {
+    this._QuizzesService.upComingFive().subscribe((quizzes) => {
+      this.upcomingQuizzes = quizzes;
+      console.log(this.upcomingQuizzes);
+    });
+  }
+
+  onAllQuizzes(): void {
+    this._QuizzesService.allQuizzes().subscribe({
+      next: (res: any) => {
+        this.quizList = res;
+        console.log('QUIZ LIST' , this.quizList);
+      },
+      error: (err: any) => {},
+    });
+  }
+
+  completedQuizzes(): void {
+    this._QuizzesService.allQuizzes().subscribe((data: any[]) => {
+
+        this.completedQuizList = data.filter(item => item.status === 'closed');
+        console.log('COMPLETED QUIZ LIST' , this.completedQuizList);
+    });
+  }
+
 }
--- src/app/features/instructor/quizzes/services/quizzes.service.ts
+++ src/app/features/instructor/quizzes/services/quizzes.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IQuiz } from '../../models/instructor';
+import { IQuizzes } from '../models/iQuizzes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuizzesService {
+  constructor(private _HttpClient: HttpClient) {}
 
-  constructor() { }
+  upComingFive(): Observable<IQuiz[]> {
+    return this._HttpClient.get<IQuiz[]>('quiz/incomming');
+  }
+
+  allQuizzes(): Observable<IQuizzes[]> {
+    return this._HttpClient.get<IQuizzes[]>('quiz');
+  }
 }
