d779cb6360a649836bed50a106cb2400113e9fd3
abdalla114 <108231471+abdalla114@users.noreply.github.com>
2024-07-02 15:16:07 +0300

    changeePass

--- src/app/features/auth/components/change-password/change-password.component.ts
+++ src/app/features/auth/components/change-password/change-password.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { ToastrService } from 'src/app/common/helper-services/toastr.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-change-password',
@@ -12,7 +13,7 @@ export class ChangePasswordComponent {
   hidePassword:boolean=true;
   hidePassword1:boolean=true;
   hidePassword2:boolean=true;
-  constructor(private _AuthService:AuthService,private _ToastrService:ToastrService,private _FormBuilder:FormBuilder){}
+  constructor(private _AuthService:AuthService,private _ToastrService:ToastrService,private _FormBuilder:FormBuilder,private _Router:Router){}
   
   changeForm:FormGroup = this._FormBuilder.group({
     password:['',[Validators.required,Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{6,}$/)]],
@@ -25,6 +26,7 @@ export class ChangePasswordComponent {
       next:res=>{
         console.log(res);
         this._ToastrService.Success('Password updated successfully')
+        this._Router.navigate(['/dashboard'])
         
       },error:err=>{
         console.log(err);
