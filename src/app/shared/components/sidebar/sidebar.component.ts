36049868a35bc8c41d28ec35c973bcd38df4f7cb
Seham1199 <sehamnabawi@gmail.com>
2024-07-03 03:26:25 +0200

    edit-sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Role } from './../../../core/Enums/Role.enum';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IMenu } from 'src/app/core/model/global';
 
 @Component({
@@ -12,62 +12,63 @@ export class SidebarComponent {
   @Output() isOpenedValue = new EventEmitter<boolean>();
   isOpened:boolean = true;
 
+  @Input() collapse!: boolean
 
-  isInstructor():boolean{
-    return localStorage.getItem('role') == Role.Instructor? true : false ;
-   }
+  // isInstructor():boolean{
+  //   return localStorage.getItem('role') == Role.Instructor? true : false ;
+  //  }
 
-   isStudent():boolean{
-     return localStorage.getItem('role') == Role.Student? true : false ;
-    }
+  //  isStudent():boolean{
+  //    return localStorage.getItem('role') == Role.Student? true : false ;
+  //   }
 
 
-  menu:IMenu[] = [
-    {
-      text: 'Dashboard',
-      icon: 'bi bi-house-door',
-      link: '/dashboard/instructor/home',
-      isActive: this.isInstructor() || this.isStudent()
-    },
-    {
-    text:'Groups' ,
-    icon: 'bi bi-people' ,
-    link:'/dashboard/instructor/groups',
-    isActive: this.isInstructor() ,
-    } ,
-    {
-      text:'Quizzes' ,
-      icon: 'bi bi-alarm' ,
-      link:'/dashboard/instructor/quizzes',
-      isActive: this.isInstructor() || this.isStudent()
-      } ,
+  // menu:IMenu[] = [
+  //   {
+  //     text: 'Dashboard',
+  //     icon: 'bi bi-house-door',
+  //     link: '/dashboard/instructor/home',
+  //     isActive: this.isInstructor() || this.isStudent()
+  //   },
+  //   {
+  //   text:'Groups' ,
+  //   icon: 'bi bi-people' ,
+  //   link:'/dashboard/instructor/groups',
+  //   isActive: this.isInstructor() ,
+  //   } ,
+  //   {
+  //     text:'Quizzes' ,
+  //     icon: 'bi bi-alarm' ,
+  //     link:'/dashboard/instructor/quizzes',
+  //     isActive: this.isInstructor() || this.isStudent()
+  //     } ,
 
-      {
-        text:'Students' ,
-        icon: 'bi bi-people' ,
-        link:'/dashboard/instructor/students/students-list',
-        isActive: this.isInstructor() || this.isStudent()
-        } ,
+  //     {
+  //       text:'Students' ,
+  //       icon: 'bi bi-people' ,
+  //       link:'/dashboard/instructor/students/students-list',
+  //       isActive: this.isInstructor() || this.isStudent()
+  //       } ,
 
-        {
-          text:'Questions' ,
-          icon: 'bi bi-clipboard2-check' ,
-          link:'/dashboard/instructor/questions',
-          isActive: this.isInstructor()
-          } ,
-      {
-        text:'Results' ,
-        icon: 'bi bi-journal-richtext' ,
-        link:'/dashboard/instructor/results',
-        isActive: this.isInstructor() || this.isStudent()
-        } ,
-        {
-          text:'Help' ,
-          icon: 'bi bi-question-circle' ,
-          link:'/dashboard/instructor/help',
-          isActive: this.isInstructor() || this.isStudent()
-          } ,
-  ]
+  //       {
+  //         text:'Questions' ,
+  //         icon: 'bi bi-clipboard2-check' ,
+  //         link:'/dashboard/instructor/questions',
+  //         isActive: this.isInstructor()
+  //         } ,
+  //     {
+  //       text:'Results' ,
+  //       icon: 'bi bi-journal-richtext' ,
+  //       link:'/dashboard/instructor/results',
+  //       isActive: this.isInstructor() || this.isStudent()
+  //       } ,
+  //       {
+  //         text:'Help' ,
+  //         icon: 'bi bi-question-circle' ,
+  //         link:'/dashboard/instructor/help',
+  //         isActive: this.isInstructor() || this.isStudent()
+  //         } ,
+  // ]
 
   onClicked() {
     this.isOpened = !this.isOpened;
