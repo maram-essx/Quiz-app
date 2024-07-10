33d8fcb8100cdaec91ef88426bc90a7599d048c2
IsrAhmad <Israa.099@gmail.com>
2024-07-10 15:46:22 +0300

    view-profile

--- src/app/shared/components/view-profile/view-profile.component.ts
+++ src/app/shared/components/view-profile/view-profile.component.ts
@@ -43,7 +43,9 @@ export class ViewProfileComponent implements OnInit {
     private _AuthService: AuthService,
     private _ActivatedRoute: ActivatedRoute,
     private _Router: Router
-  ) {}
+  ) {
+    this.getCurrentUser();
+  }
 
   ngOnInit(): void {
     this.userId = this._ActivatedRoute.snapshot.params['id'];
@@ -52,12 +54,11 @@ export class ViewProfileComponent implements OnInit {
       // from users page
       this.isViewUser = true;
     }
-    this.getCurrentUser();
   }
 
   getCurrentUser(): void {
     const role = localStorage.getItem('role');
-    const first_name = localStorage.getItem('firs_name');
+    const first_name = localStorage.getItem('first_name');
     const last_name = localStorage.getItem('last_name');
     const email = localStorage.getItem('email');
     const status = localStorage.getItem('status');
@@ -68,6 +69,10 @@ export class ViewProfileComponent implements OnInit {
       this.last_name = last_name;
       this.email = email;
       this.status = status;
+
+      console.log('IF CONDITION FIRES');
+      console.log('PROFILE DATA: ', this.role, this.first_name, this.last_name, this.email, this.status);
+
     } else {
     }
   }
